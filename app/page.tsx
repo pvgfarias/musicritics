8db26df73f63f2050eff7fda0c4dd4b63a736eb7
devpve <paulovictor@live.fr>
2026-07-21 11:33:05 -0300

    refactor: :heavy_minus_sign: removing some dependecies, reverting projec to initial state

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import Hero from '../components/home/hero';
-import Login from '../components/home/login';
-
 export default function Home() {
   return (
     <main className='flex flex-row grow'>
-      <Hero />
-      <Login />
+      <h1>Hey</h1>
     </main>
   );
 }
