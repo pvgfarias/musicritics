7834e41e651c5565c670112ca9344cf02481ab69
devpve <paulovictor@live.fr>
2026-07-22 11:38:52 -0300

    feat: :sparkles: creating login and register pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,17 +6,17 @@ export default function Login() {
   return (
     <section className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-6 text-center text-white bg-amber-50 dark:bg-slate-950'>
       <motion.form
-        initial={{ opacity: 0, x: 50 }}
-        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
         viewport={{ once: true }}
         // onSubmit={handleSubmit}
-        className='space-y-6'
+        className='space-y-6 md:w-1/5'
       >
         <div>
           <label
             htmlFor='email'
-            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2'
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 text-left'
           >
             Email
           </label>
@@ -26,14 +26,15 @@ export default function Login() {
             name='email'
             // value={formData.name}
             // onChange={handleChange}
+            placeholder='email@email.com'
             required
-            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-emerald-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white'
+            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white '
           />
         </div>
         <div>
           <label
             htmlFor='password'
-            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2'
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 text-left'
           >
             Password
           </label>
@@ -41,13 +42,20 @@ export default function Login() {
             type='password'
             id='name'
             name='name'
-            placeholder='******'
+            placeholder='**********'
             // value={formData.name}
             // onChange={handleChange}
             required
-            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-emerald-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white'
+            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white'
           />
         </div>
+        <button
+          type='submit'
+          // disabled={isSubmitting}
+          className='w-full flex items-center justify-center px-6 py-3 bg-linear-to-r from-amber-600 to-amber-500 text-white rounded-lg font-semibold hover:opacity-90 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed'
+        >
+          Login
+        </button>
       </motion.form>
     </section>
   );
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,80 @@
-export default function Register() {
-  return <h1>Hello</h1>;
+'use client';
+
+import { motion } from 'motion/react';
+
+export default function Login() {
+  return (
+    <section className='relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-6 text-center text-white bg-amber-50 dark:bg-slate-950'>
+      <motion.form
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+        // onSubmit={handleSubmit}
+        className='space-y-6 md:w-1/5'
+      >
+        <div>
+          <label
+            htmlFor='email'
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 text-left'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            // value={formData.name}
+            // onChange={handleChange}
+            placeholder='email@email.com'
+            required
+            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white '
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='password'
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 text-left'
+          >
+            Password
+          </label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            placeholder='**********'
+            // value={formData.name}
+            // onChange={handleChange}
+            required
+            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='confirmPassword'
+            className='block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 text-left'
+          >
+            Confirm Password
+          </label>
+          <input
+            type='password'
+            id='confirmPassword'
+            name='confirmPassword'
+            placeholder='**********'
+            // value={formData.name}
+            // onChange={handleChange}
+            required
+            className='w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent bg-white dark:bg-slate-700 text-gray-900 dark:text-white'
+          />
+        </div>
+        <button
+          type='submit'
+          // disabled={isSubmitting}
+          className='w-full flex items-center justify-center px-6 py-3 bg-linear-to-r from-amber-600 to-amber-500 text-white rounded-lg font-semibold hover:opacity-90 transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed'
+        >
+          Register
+        </button>
+      </motion.form>
+    </section>
+  );
 }
